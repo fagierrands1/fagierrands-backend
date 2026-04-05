922ec0256db8f0221f0595892110c5a25157d3b8
fagierrands1 <fagierrands1@gamil.com>
2026-04-05 23:23:00 +0300

    hardcode admin superuser credentials for reder login

---
+++ accounts/management/commands/create_admin.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from accounts.models import User
+import os
+
+class Command(BaseCommand):
+    help = 'Create superuser from environment variables'
+
+    def handle(self, *args, **options):
+        username = os.environ.get('ADMIN_USERNAME', 'admin')
+        email = os.environ.get('ADMIN_EMAIL', 'admin@fagierrands.com')
+        password = os.environ.get('ADMIN_PASSWORD', 'admin123')
+        
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS(f'Superuser {username} created'))
+        else:
+            self.stdout.write(self.style.WARNING(f'Superuser {username} already exists'))
