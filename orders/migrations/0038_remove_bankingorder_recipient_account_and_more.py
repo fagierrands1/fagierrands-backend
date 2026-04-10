8e615dc91612be89f50e42992ae444eb7b69f2a2
fagierrands1 <fagierrands1@gamil.com>
2026-04-10 16:19:28 +0300

    Fix migration: safely remove fields only if they exist

--- orders/migrations/0038_remove_bankingorder_recipient_account_and_more.py
+++ orders/migrations/0038_remove_bankingorder_recipient_account_and_more.py
@@ -3,6 +3,24 @@
 from django.db import migrations, models
 
 
+def remove_fields_if_exist(apps, schema_editor):
+    """Safely remove fields only if they exist"""
+    from django.db import connection
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT column_name 
+            FROM information_schema.columns 
+            WHERE table_name='orders_bankingorder' 
+            AND column_name IN ('recipient_account', 'recipient_name')
+        """)
+        existing_columns = [row[0] for row in cursor.fetchall()]
+        
+        if 'recipient_account' in existing_columns:
+            cursor.execute('ALTER TABLE orders_bankingorder DROP COLUMN recipient_account')
+        if 'recipient_name' in existing_columns:
+            cursor.execute('ALTER TABLE orders_bankingorder DROP COLUMN recipient_name')
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -10,14 +28,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='bankingorder',
-            name='recipient_account',
-        ),
-        migrations.RemoveField(
-            model_name='bankingorder',
-            name='recipient_name',
-        ),
+        migrations.RunPython(remove_fields_if_exist, migrations.RunPython.noop),
         migrations.AlterField(
             model_name='orderprepayment',
             name='payment_method',
